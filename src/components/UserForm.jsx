939decd2f3a7f0434c3d7c37ab637734bf8aab39
notebook <julioscalves@gmail.com>
2021-12-15 06:36:05 -0300

    states reorder

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -20,9 +20,9 @@ const states = [
     "AC", "AL", "AP", "AM", "BA",
     "CE", "DF", "ES", "GO", "MA", 
     "MT", "MS", "MG", "PA", "PB", 
-    "PR", "PI", "RJ", "RN", "RS", 
-    "RO", "RR", "SC", "SP", "SE", 
-    "TO", "PE",
+    "PE", "PR", "PI", "RJ", "RN", 
+    "RS", "RO", "RR", "SC", "SP", 
+    "SE", "TO"
 ]
 
 function UserForm(props) {
